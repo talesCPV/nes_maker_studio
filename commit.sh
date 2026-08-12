a2e184595833f23b0f3e1081a9c8db44233a6b40
talesCPV <tales@flexibus.com.br>
2026-08-11 21:13:08 -0300

    by_script

--- commit.sh
+++ commit.sh
@@ -17,11 +17,11 @@ then
     git add commit.sh
     git add index.html
     git add hello.nms
-    
+
     git commit -m "by_script"
 
-    git branch -M main
-    git remote add origin git@github.com:talesCPV/nes_maker_studio.git
+#    git branch -M main
+#    git remote add origin git@github.com:talesCPV/nes_maker_studio.git
     git remote set-url origin git@github.com:talesCPV/nes_maker_studio.git
 
     git push -u -f origin main
